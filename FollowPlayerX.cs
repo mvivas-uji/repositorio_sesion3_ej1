30b1a223513316f15afa8ba3e328f4227daa1967
mvivas-uji <113539987+mvivas-uji@users.noreply.github.com>
2022-09-28 18:08:54 +0200

    Add files via upload

---
+++ FollowPlayerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+/*{
+    public GameObject Player;
+    //private Vector3 offset = new Vector3(26.76f,2.12f, -23.33f);
+    Vector3 originalCamPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalCamPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + originalCamPos;
+    }
+}*/
+{
+    public Transform targetObject; 
+    private Vector3 initalOffset;
+    private Vector3 cameraPosition;
+
+    void Start()
+    {
+        initalOffset = transform.position - targetObject.position;
+    }
+
+    void FixedUpdate()
+    {
+        cameraPosition = targetObject.position + initalOffset;
+        transform.position = cameraPosition;
+    }
+}
---
+++ Propeller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Aquí se define la clase "propeller" y sus animaciones
+public class Propeller : MonoBehaviour
+{
+    public float propSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        propSpeed = 5000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * propSpeed * Time.deltaTime);
+    }
+}
